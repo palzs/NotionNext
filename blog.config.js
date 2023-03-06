04b4ee6dfbfb977e80d7873c04bf780dd3d51075
palzs <37971999+palzs@users.noreply.github.com>
2023-03-06 21:51:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,9 +25,9 @@ const BLOG = {
   // 网站字体
   FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-serif', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   FONT_URL: [// 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
-    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@500&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@500&display=swap'
+    'https://fonts.googleapis.com/css?family=Bitter',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@500',
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@500'
   ],
   FONT_SANS: [// 无衬线字体 例如'LXGW WenKai'
     'Microsoft YaHei', '"PingFang SC"', '-apple-system', 'BlinkMacSystemFont', '"Hiragino Sans GB"',
